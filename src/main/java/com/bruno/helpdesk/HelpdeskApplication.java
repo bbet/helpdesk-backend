d54483284065c5cf8672a8c1d7840253af786fac
Bruno Bet <cesarbarellibet@gmail.com>
2022-05-01 19:14:46 -0300

    ajustes

--- src/main/java/com/bruno/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/bruno/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,33 @@
 package com.bruno.helpdesk;
 
+import java.sql.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bruno.helpdesk.domain.Chamado;
+import com.bruno.helpdesk.domain.Cliente;
+import com.bruno.helpdesk.domain.Tecnico;
+import com.bruno.helpdesk.domain.enums.Perfil;
+import com.bruno.helpdesk.domain.enums.Prioridade;
+import com.bruno.helpdesk.domain.enums.Status;
+import com.bruno.helpdesk.repositories.ChamadoRepository;
+import com.bruno.helpdesk.repositories.ClienteRepository;
+import com.bruno.helpdesk.repositories.TecnicoRepository;
+
 @SpringBootApplication
 public class HelpdeskApplication {
+	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
+
+
+		
 
 }
